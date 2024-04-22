0cc73b6bd081f1a6ec87b3903b3c856fb66595a9
Cecilie <cepe0001@stud.kea.dk>
2024-04-22 19:14:47 +0200

    ryder op

--- src/js/scroll.js
+++
@@ -1,9 +0,0 @@
-const svg = document.querySelector("svg.redline");
-const path = svg.querySelector("path");
-
-const scroll = () => {
-  path.style.strokeDasharray = `60`;
-};
-
-scroll();
-window.addEventListener("scroll", scroll);
